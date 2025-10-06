c71b1489ac9fb38569c2b27eb1e9babe134b6e27
Rafał <rav>
2025-10-06 16:13:46 +0200

    LOTTO

---
+++ Lab1/Lotto.java
@@ -0,0 +1,19 @@
+import java.until.Random;
+
+import java.util.HashSet;
+
+public class Lotto{
+	public static void main(String[] args){
+		Random randomNumbers = new Random();
+		
+		HashSet<String> liczby = new HashSet<String>();
+
+		for(int i =0;  i < 6; i++){
+			liczby.add(rand.nextInt(10));
+
+	
+			}
+		
+		System.out.println("Liczby: "+ liczby);
+		}
+	}
