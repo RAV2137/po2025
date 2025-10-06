43ef60c88441f9241cd33a358e08031b35a614e4
Rafał <rav>
2025-10-06 15:53:36 +0200

    Zajęcia 1: Hello i Choinka

---
+++ Lab1/Hallo.java
@@ -0,0 +1,12 @@
+public class Hello{
+	public static void main(String[] args){
+		
+		int n=Integer.parseInt(args[0]);
+		for(int j=0; j<n ; j++){
+			for(int i=0; i< j ; i++){
+				System.out.print("*");
+				}
+			System.out.println("*");
+			}
+		}
+	}
