b67323564e740b1c639540d8cfbdf2f080d39dd0
Rafał <rav>
2025-10-20 16:17:01 +0200

    koniec

---
+++ Lab3/src/codingbat.java
@@ -0,0 +1,18 @@
+public class codingbat {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean monkeyTrouble ( boolean aSmile, boolean bSmile){
+        if (aSmile && bSmile) {
+            return true;
+        }
+        if (!aSmile && !bSmile) {
+            return true;
+        }
+        return false;
+
+    }
+
+}
---
+++ Lab3/src/codingbatTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+public class codingbatTest {
+
+    @org.junit.Test
+    public void monkeyTrouble() {
+        codingbat test = new codingbat();
+        assertTrue(test.monkeyTrouble(true,true));
+
+
+    }
+    }
+
