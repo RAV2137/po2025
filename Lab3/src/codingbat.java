bc42ed84f7ec25e1a2b58f7518e6b247179d2c4c
Rafał <zrobione@>
2025-10-21 22:13:23 +0200

    "Lab3: CodingBat + testy jednostkowe + debugowanie"

--- Lab3/src/codingbat.java
+++ Lab3/src/codingbat.java
@@ -1,7 +1,6 @@
 public class codingbat {
 
     public static void main(String[] args) {
-
     }
 
     public boolean monkeyTrouble ( boolean aSmile, boolean bSmile){
@@ -14,5 +13,43 @@ public class codingbat {
         return false;
 
     }
+    public String front22(String str) {
+        if (str.length() ==0){
+            return str;
+        }
+        else if (str.length() == 1){
+            return str.substring(0) + str +  str.charAt(0) ;
+        }
+            return str.substring(0, 2) + str + str.substring(0, 2);
+        }
+
+    public int[] maxEnd3(int[] nums) {
+        int max=0;
+        if(nums[0]> nums[nums.length-1]){
+            max=nums[0];
+        }
+        else{
+            max = nums[nums.length-1];
+        }
+        for (int i =0; i<nums.length;i++){
+            nums[i]=max;
+        }
+        return nums;
+    }
+    public boolean hasBad(String str) {
+        char[] word= {'b','a','d'};
+        char[] str_char= str.toCharArray();
+        if (str.length()<3){
+            return false;
+        }
+        if ( str_char[0]==word[0] && str_char[1]==word[1] && str_char[2]==word[2]){
+            return true;
+        }
+        if( str.length() >= 4 && str_char[1]==word[0] && str_char[2]==word[1] && str_char[3]==word[2]){
+            return true;
+        }
+        return false;
+    }
+
 
 }
--- Lab3/src/codingbatTest.java
+++ Lab3/src/codingbatTest.java
@@ -2,12 +2,53 @@ import static org.junit.Assert.*;
 
 public class codingbatTest {
 
-    @org.junit.Test
+    @org.junit.Test     //ważne bo nie wykona się test
     public void monkeyTrouble() {
         codingbat test = new codingbat();
         assertTrue(test.monkeyTrouble(true,true));
+        assertTrue(test.monkeyTrouble(false,false));
+        assertFalse(test.monkeyTrouble(false,true));
+
+    }
+    @org.junit.Test     //ważne bo nie wykona się test
+    public void front22() {
+        codingbat test = new codingbat();
+
+        String result= test.front22("Ha");
+        assertEquals("HaHaHa",result);
+
+        result= test.front22("");
+        assertEquals("",result);
+
+        result= test.front22("a");
+        assertEquals("aaa",result);
+    }
+    @org.junit.Test     //ważne bo nie wykona się test
+    public void maxEnd3() {
+        codingbat test = new codingbat();
+        int[] tab1 = {1,2,3};
+        int[] result1= test.maxEnd3(tab1);
+        assertArrayEquals(new int[] {3,3,3},result1);
+
+        int[] tab2 = {2, 11, 3};
+        int[] result2= test.maxEnd3(tab2);
+        assertArrayEquals(new int[] {3,3,3},result2);
+
+        int[] tab3 = {3, 11, 11};
+        int[] result3= test.maxEnd3(tab3);
+        assertArrayEquals(new int[] {11,11,11},result3);
 
 
     }
+    @org.junit.Test     //ważne bo nie wykona się test
+    public void test_hasBad(){
+        codingbat test = new codingbat();
+        assertTrue(test.hasBad("bad"));
+        assertFalse(test.hasBad("bedsa"));
+        assertTrue(test.hasBad("xbad"));
+
+
+    }
+
     }
 
