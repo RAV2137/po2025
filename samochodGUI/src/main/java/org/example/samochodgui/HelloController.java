7ee7176eaf02c7ce6abbfe966fb5e72d0759b297
Rafał <rav>
2025-11-24 17:13:12 +0100

    Lab07

--- samochodGUI/src/main/java/org/example/samochodgui/HelloController.java
+++ samochodGUI/src/main/java/org/example/samochodgui/HelloController.java
@@ -1,14 +1,34 @@
 package org.example.samochodgui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    public Button stopButton;
+
+    @FXML
+    public Button startButton;
+
+    public Button ZwiększbiegButton;
+    public Button ZmniejszbiegButton;
+    public Button dodaj_gazuButton;
+    public Button ujmij_gazuButton;
+    public Button nacisnijButton;
+    public Button zwolnijButton;
+//    @FXML
+//    private Label welcomeText;
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Welcome to JavaFX Application!");}
+
+
+    @FXML
+    public void onstartButton() {
+        System.out.println("Wlaczono silnik");}
+
+
+
 }
